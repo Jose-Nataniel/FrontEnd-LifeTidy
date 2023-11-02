15645021814fbffe26e45346d520743a519aa0ae
Renanlucass <renanlucas0014@gmail.com>
2023-11-01 21:07:14 -0300

    update3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Login from './components/pages/Login';
-import Cadastro from './components/pages/Cadastro';
-import Inicio from './components/pages/Inicio';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-  <div className="App">
-    <Switch>
-      <Route path="/" exact component={Inicio} />
-      <Route path="/cadastro" component={Cadastro} />
-      <Route path="/login" component={Login} />
-    </Switch>
-  </div>
-</Router>
-  )
 
-  }
-export default App;
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Inicio />} />
+          <Route path="Cadastro" element={<Cadastro />} />
+          <Route path="Login" element={<Login />} />
+
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <Outlet />
+    </div>
+  );
+}
+
+
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import './Global.css'; 
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-
+import './Global.css';
+import {  Link } from "react-router-dom";
 
 
 
 function Inicio() {
   return (
-    <Router>
     <div className="body-tela-apresentacao">
       <header className="header-tela-apresentacao">
         <nav className="nav-apresentacao">
@@ -22,155 +20,155 @@ function Inicio() {
             </a>
           </div>
           <div className="div-nav-apresentacao">
-          <Link to="/cadastro" className="link-nav-tela-apresentacao">
-                CADASTRO
-              </Link>
-              <Link to="/login" className="link-nav-tela-apresentacao login-apresentacao">
-                LOGIN
-              </Link>
+            <Link to="/cadastro" className="link-nav-tela-apresentacao">
+              CADASTRO
+            </Link>
+            <Link to="/login" className="link-nav-tela-apresentacao login-apresentacao">
+              LOGIN
+            </Link>
           </div>
         </nav>
       </header>
-    
-      
+
+
       <main>
-      <div className="container-principal">
-        <div className="texts-apresentacao">
-          <h1 className="h1-text-apresentacao">ORGANIZE SUA ROTINA COM O LIFETIDY</h1>
-          <p className="p-text-apresentacao">ALCANCE O FOCO E O CONTROLE DA SUA AGENDA</p>
+        <div className="container-principal">
+          <div className="texts-apresentacao">
+            <h1 className="h1-text-apresentacao">ORGANIZE SUA ROTINA COM O LIFETIDY</h1>
+            <p className="p-text-apresentacao">ALCANCE O FOCO E O CONTROLE DA SUA AGENDA</p>
+          </div>
+          <div className="container-divs">
+            <img className="img-exibicao-tela-inicial" src="/img/img-landingPage.png" alt="exibicaoTelaInicial" />
+          </div>
+          <div className="div-informacoes">
+            <h2 id="pontoOne" className="h2-informacoes">COM LIFETIDY VOCÊ PODE FAZER MAIS!</h2>
+            <div className="div-todas-informacoes">
+              <div className="div-informacoes-left-right">
+                <div className="div-informacoes-com-icones">
+                  <img className="icons-apresentacao" src="/img/adicionar.png" alt="icone-adicionar" />
+                  <p className="texts-informacoes">
+                    ADICIONE DIVERSAS TAREFAS, QUANTAS VEZES FOREM NECESSÁRIAS,
+                    PERSONALIZANDO AS CATEGORIAS DE ACORDO COM A NECESSIDADE.
+                  </p>
+                </div>
+                <div className="div-informacoes-com-icones">
+                  <img className="icons-apresentacao" src="/img/excluir.png" alt="icone-excluir" />
+                  <p className="texts-informacoes">
+                    QUE TAL EXCLUIR TAREFAS QUE NÃO SERÃO FEITAS? COM A OPÇÃO
+                    “EXCLUIR TAREFA”, VOCÊ OTIMIZA AO MÁXIMO SUA LISTA DE TAREFAS.
+                  </p>
+                </div>
+              </div>
+              <div className="div-informacoes-left-right">
+                <div className="div-informacoes-com-icones">
+                  <img className="icons-apresentacao" src="/img/notificacoes.png" alt="icone-notificações" />
+                  <p className="texts-informacoes">
+                    ESQUECEU DO SEU COMPROMISSO OU DA SUA TAREFA? A GENTE TE
+                    LEMBRA! COM NOTIFICAÇÕES INTERNAS E EM POP UP, ESQUECER NÃO SERÁ PROBLEMA.
+                  </p>
+                </div>
+                <div className="div-informacoes-com-icones">
+                  <img className="icons-apresentacao" src="/img/rendimento.png" alt="icone-rendimento" />
+                  <p className="texts-informacoes">
+                    QUER SABER O SEU RENDIMENTO PESSOAL? AQUI A GENTE TE MOSTRA,
+                    COM GRÁFICOS DE RENDIMENTO SOBRE TAREFAS PENDENTES, FINALIZADAS E A FAZER, VOCÊ VERÁ OS
+                    RESULTADOS EM PRIMEIRA MÃO.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="container-divs">
-          <img className="img-exibicao-tela-inicial" src="/img/img-landingPage.png" alt="exibicaoTelaInicial" />
-        </div>
-        <div className="div-informacoes">
-          <h2 id="pontoOne" className="h2-informacoes">COM LIFETIDY VOCÊ PODE FAZER MAIS!</h2>
+
+        <div className="container-tarefas">
+          <h2 className="h2-informacoes">ADICIONE TAREFAS DE MANEIRA FÁCIL E RÁPIDA!</h2>
           <div className="div-todas-informacoes">
-            <div className="div-informacoes-left-right">
-              <div className="div-informacoes-com-icones">
-                <img className="icons-apresentacao" src="/img/adicionar.png" alt="icone-adicionar" />
-                <p className="texts-informacoes">
-                  ADICIONE DIVERSAS TAREFAS, QUANTAS VEZES FOREM NECESSÁRIAS,
-                  PERSONALIZANDO AS CATEGORIAS DE ACORDO COM A NECESSIDADE.
-                </p>
-              </div>
-              <div className="div-informacoes-com-icones">
-                <img className="icons-apresentacao" src="/img/excluir.png" alt="icone-excluir" />
-                <p className="texts-informacoes">
-                  QUE TAL EXCLUIR TAREFAS QUE NÃO SERÃO FEITAS? COM A OPÇÃO
-                  “EXCLUIR TAREFA”, VOCÊ OTIMIZA AO MÁXIMO SUA LISTA DE TAREFAS.
-                </p>
-              </div>
-            </div>
-            <div className="div-informacoes-left-right">
-              <div className="div-informacoes-com-icones">
-                <img className="icons-apresentacao" src="/img/notificacoes.png" alt="icone-notificações" />
-                <p className="texts-informacoes">
-                  ESQUECEU DO SEU COMPROMISSO OU DA SUA TAREFA? A GENTE TE
-                  LEMBRA! COM NOTIFICAÇÕES INTERNAS E EM POP UP, ESQUECER NÃO SERÁ PROBLEMA.
-                </p>
-              </div>
-              <div className="div-informacoes-com-icones">
-                <img className="icons-apresentacao" src="/img/rendimento.png" alt="icone-rendimento" />
-                <p className="texts-informacoes">
-                  QUER SABER O SEU RENDIMENTO PESSOAL? AQUI A GENTE TE MOSTRA,
-                  COM GRÁFICOS DE RENDIMENTO SOBRE TAREFAS PENDENTES, FINALIZADAS E A FAZER, VOCÊ VERÁ OS
-                  RESULTADOS EM PRIMEIRA MÃO.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="container-tarefas">
-        <h2 className="h2-informacoes">ADICIONE TAREFAS DE MANEIRA FÁCIL E RÁPIDA!</h2>
-        <div className="div-todas-informacoes">
-          <div className="div-todas-informacoes-tarefas-left">
-            <ul>
-              <li className="texts-informacoes-listas">PERSONALIZE SUAS TAREFAS USANDO A SELEÇÃO DE CATEGORIAS E
-                GRAU DE IMPORTÂNCIA.</li>
-              
-              <li className="texts-informacoes-listas">DEFINA PRAZOS PRECISOS PARA SUAS TAREFAS, UTILIZANDO A DATA
-                E HORA DESEJADAS.</li>
-            </ul>
-            <img className="img-exemplo-adicionar-tarefa-wiki" src="/img/adicionarTarefaWiki.png"
-              alt="Imagem-exemplo-adicionar-tarefa-wiki" />
-          </div>
-          <div className="div-todas-informacoes-tarefas-right">
-            <img className="img-exemplo-adicionar-tarefa" src="/img/adicionarTarefa.png"
-              alt="Imagem-exemplo-adicionar-tarefa" />
-          </div>
-        </div>
-      </div>
-
-      <div className="container-notificacoes">
-        <h2 className="h2-informacoes">INTERFACE DE NOTIFICAÇÕES DE FÁCIL COMPREENSÃO!</h2>
-        <div className="div-todas-informacoes">
-          <div className="div-todas-informacoes-tarefas-left">
-            <ul>
-              <li className="texts-informacoes-listas">NOTIFICAÇÕES ORDENADAS DE ACORDO COM O GRAU DE URGÊNCIA OU
-                DATA.</li>
-              
-              <li className="texts-informacoes-listas">INTERFACE INTUITIVA, AMIGÁVEL E DE FÁCIL ENTENDIMENTO.</li>
-            </ul>
-            <img className="img-exemplo-adicionar-tarefa-wiki" src="/img/notificacoesWiki.png"
-              alt="Imagem-exemplo-adicionar-tarefa-wiki" />
-          </div>
-          <div className="div-todas-informacoes-tarefas-right">
-            <img className="img-exemplo-adicionar-tarefa" src="/img/notificacoesIndex.png"
-              alt="Imagem-exemplo-adicionar-tarefa" />
-          </div>
-        </div>
-      </div>
-
-      <div className="container-informativo-redimento">
-        <h2 className="h2-informacoes">TENHA ACESSO AO SEU INFORMATIVO DE RENDIMENTOS</h2>
-        <div className="div-todas-informacoes">
-          <div className="div-todas-informacoes-tarefas-left">
-            <div className="div-texts-informacoes-listas">
+            <div className="div-todas-informacoes-tarefas-left">
               <ul>
-                <li className="texts-informacoes-listas">PERSONALIZE SUAS TAREFAS USANDO A SELEÇÃO DE CATEGORIAS
-                  E GRAU DE IMPORTÂNCIA.</li>
-                
-                <li className="texts-informacoes-listas">DEFINA PRAZOS PRECISOS PARA SUAS TAREFAS, UTILIZANDO A
+                <li className="texts-informacoes-listas">PERSONALIZE SUAS TAREFAS USANDO A SELEÇÃO DE CATEGORIAS E
+                  GRAU DE IMPORTÂNCIA.</li>
+
+                <li className="texts-informacoes-listas">DEFINA PRAZOS PRECISOS PARA SUAS TAREFAS, UTILIZANDO A DATA
+                  E HORA DESEJADAS.</li>
+              </ul>
+              <img className="img-exemplo-adicionar-tarefa-wiki" src="/img/adicionarTarefaWiki.png"
+                alt="Imagem-exemplo-adicionar-tarefa-wiki" />
+            </div>
+            <div className="div-todas-informacoes-tarefas-right">
+              <img className="img-exemplo-adicionar-tarefa" src="/img/adicionarTarefa.png"
+                alt="Imagem-exemplo-adicionar-tarefa" />
+            </div>
+          </div>
+        </div>
+
+        <div className="container-notificacoes">
+          <h2 className="h2-informacoes">INTERFACE DE NOTIFICAÇÕES DE FÁCIL COMPREENSÃO!</h2>
+          <div className="div-todas-informacoes">
+            <div className="div-todas-informacoes-tarefas-left">
+              <ul>
+                <li className="texts-informacoes-listas">NOTIFICAÇÕES ORDENADAS DE ACORDO COM O GRAU DE URGÊNCIA OU
+                  DATA.</li>
+
+                <li className="texts-informacoes-listas">INTERFACE INTUITIVA, AMIGÁVEL E DE FÁCIL ENTENDIMENTO.</li>
+              </ul>
+              <img className="img-exemplo-adicionar-tarefa-wiki" src="/img/notificacoesWiki.png"
+                alt="Imagem-exemplo-adicionar-tarefa-wiki" />
+            </div>
+            <div className="div-todas-informacoes-tarefas-right">
+              <img className="img-exemplo-adicionar-tarefa" src="/img/notificacoesIndex.png"
+                alt="Imagem-exemplo-adicionar-tarefa" />
+            </div>
+          </div>
+        </div>
+
+        <div className="container-informativo-redimento">
+          <h2 className="h2-informacoes">TENHA ACESSO AO SEU INFORMATIVO DE RENDIMENTOS</h2>
+          <div className="div-todas-informacoes">
+            <div className="div-todas-informacoes-tarefas-left">
+              <div className="div-texts-informacoes-listas">
+                <ul>
+                  <li className="texts-informacoes-listas">PERSONALIZE SUAS TAREFAS USANDO A SELEÇÃO DE CATEGORIAS
+                    E GRAU DE IMPORTÂNCIA.</li>
+
+                  <li className="texts-informacoes-listas">DEFINA PRAZOS PRECISOS PARA SUAS TAREFAS, UTILIZANDO A
+                    DATA E HORA DESEJADAS.</li>
+                </ul>
+              </div>
+            </div>
+            <div className="div-todas-informacoes-tarefas-right">
+              <img className="img-exemplo-adicionar-tarefa" src="/img/INFORMATICO DE RENDIMENTO.png"
+                alt="Imagem-exemplo-adicionar-tarefa" />
+            </div>
+          </div>
+        </div>
+
+        <div className="container-notificacoes container-marcadores">
+          <h2 className="h2-informacoes">ADICIONE MARCADORES E GERÊNCIE SUAS TAREFAS!</h2>
+          <div className="div-todas-informacoes">
+            <div className="div-todas-informacoes-tarefas-left">
+              <ul>
+                <li className="texts-informacoes-listas">ADICIONE TAREFAS DE ACORDO COM A SUA NECESSIDAE, AGRUPE E
+                  SEPARE DE FORMA RÁPIDA E FÁCIL.</li>
+
+                <li className="texts-informacoes-listas">DEFINA PRAZOS PRECISOS PARA SEUS MARCADORES, UTILIZANDO A
                   DATA E HORA DESEJADAS.</li>
               </ul>
             </div>
-          </div>
-          <div className="div-todas-informacoes-tarefas-right">
-            <img className="img-exemplo-adicionar-tarefa" src="/img/INFORMATICO DE RENDIMENTO.png"
-              alt="Imagem-exemplo-adicionar-tarefa" />
-          </div>
-        </div>
-      </div>
-
-      <div className="container-notificacoes container-marcadores">
-        <h2 className="h2-informacoes">ADICIONE MARCADORES E GERÊNCIE SUAS TAREFAS!</h2>
-        <div className="div-todas-informacoes">
-          <div className="div-todas-informacoes-tarefas-left">
-            <ul>
-              <li className="texts-informacoes-listas">ADICIONE TAREFAS DE ACORDO COM A SUA NECESSIDAE, AGRUPE E
-                SEPARE DE FORMA RÁPIDA E FÁCIL.</li>
-              
-              <li className="texts-informacoes-listas">DEFINA PRAZOS PRECISOS PARA SEUS MARCADORES, UTILIZANDO A
-                DATA E HORA DESEJADAS.</li>
-            </ul>
-          </div>
-          <div className="div-todas-informacoes-tarefas-right">
-            <img className="img-exemplo-adicionar-tarefa" src="/img/ADICIONAR MARCADOR.png"
-              alt="Imagem-exemplo-adicionar-tarefa" />
-          </div>
-          <div className="div-texts-informacoes-listas">
-            <h2>O QUE SÃO MARCADORES?</h2>
-            <p>MARCADORES SÃO COMO UMA ESPÉCIE DE TÍTULO, NA QUAL O USUÁRIO PODE SEPARAR SUAS TAREFAS, A FIM DE
-              ORGANIZAR-SE MELHOR.
-              UM MARCADOR PODE SER O NOME DE UMA MATÉRIA, DE UM CURSO OU QUALQUER NOMECLATURA QUE FACILITE A
-              SEPARAÇÃO DAS TAREFAS
-              (EX: ATIVIDADES DE MATEMÁTICA, ATIVIDADES DA SEMANA, CSS, POO ETC.)
-            </p>
+            <div className="div-todas-informacoes-tarefas-right">
+              <img className="img-exemplo-adicionar-tarefa" src="/img/ADICIONAR MARCADOR.png"
+                alt="Imagem-exemplo-adicionar-tarefa" />
+            </div>
+            <div className="div-texts-informacoes-listas">
+              <h2>O QUE SÃO MARCADORES?</h2>
+              <p>MARCADORES SÃO COMO UMA ESPÉCIE DE TÍTULO, NA QUAL O USUÁRIO PODE SEPARAR SUAS TAREFAS, A FIM DE
+                ORGANIZAR-SE MELHOR.
+                UM MARCADOR PODE SER O NOME DE UMA MATÉRIA, DE UM CURSO OU QUALQUER NOMECLATURA QUE FACILITE A
+                SEPARAÇÃO DAS TAREFAS
+                (EX: ATIVIDADES DE MATEMÁTICA, ATIVIDADES DA SEMANA, CSS, POO ETC.)
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
       </main>
       <footer className="footer-lifetidy">
@@ -199,7 +197,6 @@ function Inicio() {
         </div>
       </footer>
     </div>
-    </Router>
   );
 }
 
